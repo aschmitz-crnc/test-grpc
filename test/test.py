d7a45ae058b80eb2f91812a6d1c0c743e576f3ec
Schmitz, Arne <arne.schmitz@cerence.com>
2025-09-19 14:42:24 +0200

    initial commit

---
+++ test/test.py
@@ -0,0 +1,17 @@
+import test_pb2
+import test_pb2_grpc
+
+class CrgServiceImpl(launcher_pb2_grpc.CrgService):
+    def __init(self):
+        pass
+
+    def Test(self, request, response):
+        print("Serving Test request")
+        return test_pb2.TestResponse()
+
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+test_service_impl = TestServiceImpl()
+test_pb2_grpc.add_TestServiceServicer_to_server(test_service_impl, server)
+server.add_insecure_port(f"0.0.0.0:12345")
+server.start()
+
