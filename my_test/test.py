12c79f6b9297e04e6c96dde2bca72e94b408339f
Schmitz, Arne <arne.schmitz@cerence.com>
2025-10-02 16:56:51 +0200

    add script

--- my_test/test.py
+++ my_test/test.py
@@ -9,9 +9,12 @@ class CrgServiceImpl(launcher_pb2_grpc.CrgService):
         print("Serving Test request")
         return test_pb2.TestResponse()
 
-server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-test_service_impl = TestServiceImpl()
-test_pb2_grpc.add_TestServiceServicer_to_server(test_service_impl, server)
-server.add_insecure_port(f"0.0.0.0:12345")
-server.start()
+def __main__():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    test_service_impl = TestServiceImpl()
+    test_pb2_grpc.add_TestServiceServicer_to_server(test_service_impl, server)
+    server.add_insecure_port(f"0.0.0.0:12345")
+    server.start()
 
+if __name__ == '__main__':
+    sys.exit(__main__())
